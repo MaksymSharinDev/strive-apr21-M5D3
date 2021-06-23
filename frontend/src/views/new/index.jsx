f06469ba114bc212a1d8079e202efa981a4f5d5c
Maksym Sharin <Maksym.sharin.work@gmail.com>
2021-06-23 16:30:30 +0200

    feat: Post can be created

--- frontend/src/views/new/index.jsx
+++ frontend/src/views/new/index.jsx
@@ -6,25 +6,59 @@ import "./styles.css";
 export default class NewBlogPost extends Component {
   constructor(props) {
     super(props);
-    this.state = { text: "" };
-    this.handleChange = this.handleChange.bind(this);
+    this.state = { text: "", title: "", category: "" };
   }
 
-  handleChange(value) {
-    this.setState({ text: value });
+  handleTextChange = (value) => {
+    this.setState({...this.state, text: value });
+  }
+  handleTitleChange= (e) => {
+    this.setState({...this.state, title: e.target.value });
+  }
+  handleCategoryChange= (e) => {
+    this.setState({ ...this.state, category: e.target.value });
   }
 
+  formatData = () => {
+    return {
+        "category": this.state.category,
+        "title": this.state.title,
+        "cover": null,
+        "readTime": {
+      "value": null,
+          "unit": null
+    },
+      "author": {
+      "name": null,
+          "avatar": null
+    },
+      "content": this.state.text,
+        "createdAt": new Date(),
+    }
+  }
+
+  sendForm = async (e) => {
+    e.preventDefault();
+    await fetch('/posts',{
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'},
+      body: JSON.stringify( this.formatData() )
+    })
+  }
   render() {
     return (
       <Container className="new-blog-container">
-        <Form className="mt-5">
+        <Form className="mt-5"
+              onSubmit={this.sendForm}
+        >
           <Form.Group controlId="blog-form" className="mt-3">
             <Form.Label>Title</Form.Label>
-            <Form.Control size="lg" placeholder="Title" />
+            <Form.Control size="lg" placeholder="Title" onChange={this.handleTitleChange}/>
           </Form.Group>
           <Form.Group controlId="blog-category" className="mt-3">
             <Form.Label>Category</Form.Label>
-            <Form.Control size="lg" as="select">
+            <Form.Control size="lg" as="select" onChange={this.handleCategoryChange}>
               <option>Category1</option>
               <option>Category2</option>
               <option>Category3</option>
@@ -36,7 +70,7 @@ export default class NewBlogPost extends Component {
             <Form.Label>Blog Content</Form.Label>
             <ReactQuill
               value={this.state.text}
-              onChange={this.handleChange}
+              onChange={this.handleTextChange}
               className="new-blog-content"
             />
           </Form.Group>
